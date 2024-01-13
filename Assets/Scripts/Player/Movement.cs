d608a5fc7b965fe020b8c73f986a855f70c21c2f
ArkanZAG <arkanalghifari22@gmail.com>
2024-01-13 21:46:42 +0700

    Sprint
    failed sprint method... hayz

--- Assets/Scripts/Player/Movement.cs
+++ Assets/Scripts/Player/Movement.cs
@@ -1,25 +1,53 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Serialization;
 
 public class Movement : MonoBehaviour
 {
     [SerializeField] private float playerMovementSpeed;
     [SerializeField] private Rigidbody playerRigidBody;
-    
+    [FormerlySerializedAs("sprintSpeed")] [SerializeField] private float playerSprintSpeed;
+    [SerializeField] private float sprintDuration;
+
+    private bool isSprinting = false;
+
     private void PlayerMovement()
-    { 
+    {
         float horizontalInput = Input.GetAxis("Horizontal"); 
         float verticalInput = Input.GetAxis("Vertical");
-
+        
         playerRigidBody.velocity = new Vector3(horizontalInput * playerMovementSpeed,
-            playerRigidBody.velocity.y, playerRigidBody.velocity.z);
-        playerRigidBody.velocity = new Vector3(playerRigidBody.velocity.x, playerRigidBody.velocity.y,
-            verticalInput * playerMovementSpeed);
+            playerRigidBody.velocity.y, verticalInput * playerMovementSpeed);
+    }
+
+    private void Sprint()
+    {
+        if (Input.GetKeyDown(KeyCode.LeftShift) && !isSprinting)
+        {
+            StartCoroutine(SprintCoroutine());
+        }
+    }
+
+    private IEnumerator SprintCoroutine()
+    {
+        isSprinting = true;
+        
+        float horizontalInput = Input.GetAxis("Horizontal"); 
+        float verticalInput = Input.GetAxis("Vertical");
+        
+        playerRigidBody.velocity = new Vector3(horizontalInput * playerSprintSpeed, playerRigidBody.velocity.y,
+            verticalInput * playerSprintSpeed);
+
+        yield return new WaitForSeconds(sprintDuration);
+
+        isSprinting = false;
     }
     
     void Update()
     {
         PlayerMovement();
+        Sprint();
     }
 }
